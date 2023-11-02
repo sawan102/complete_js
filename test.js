caff11ee2f848a299a52a5741d4fa21fba0461a2
sawan102 <149521513+sawan102@users.noreply.github.com>
2023-11-02 15:37:06 +0000

    array

--- test.js
+++ test.js
@@ -1,1 +1,10 @@
-console.log('hello');
+// console.log('hello');
+
+const arr1 = ['aa','bb','cc','dd'];
+const arr2= ['ee','gg','hh','ii'];
+
+const arr3 = arr1.concat(arr2);
+const arr4 = [...arr1,...arr2]
+
+console.log('Arr3',arr3);
+console.log('arr4',arr4);
